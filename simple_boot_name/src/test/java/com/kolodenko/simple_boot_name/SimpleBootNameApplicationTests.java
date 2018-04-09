f1f293f690c36a1e9112db35e13f4f5986a73ccc
vitaliy_kolodenko <vitaliy_kolodenko@symantec.com>
2018-04-09 14:42:19 -0400

    test fix

--- simple_boot_name/src/test/java/com/kolodenko/simple_boot_name/SimpleBootNameApplicationTests.java
+++ simple_boot_name/src/test/java/com/kolodenko/simple_boot_name/SimpleBootNameApplicationTests.java
@@ -2,11 +2,9 @@ package com.kolodenko.simple_boot_name;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class SimpleBootNameApplicationTests {
 
 	@Test
